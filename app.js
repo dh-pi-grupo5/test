7a30661a17bfbc58420ca21f4682c9e96843b120
MarcoMoutsopoulos <marcomoutsopoulos@yahoo.com.br>
2022-11-16 20:22:26 -0300

    Header,Footer, inicio da configuração

--- app.js
+++ app.js
@@ -14,10 +14,19 @@ servidor.get('/', (req, res)=>{
     return res.sendFile(__dirname + "/views/index.html");
 });
 
-servidor.get('/carrinho',(req, res)=>{
+servidor.get('/carrinho.html',(req, res)=>{
     return res.sendFile(__dirname + "/views/carrinho.html");
 });
 
+servidor.get('/produtos.html',(req, res)=>{
+    return res.sendFile(__dirname + "/views/produtos.html");
+});
+servidor.get('/quemSomos.html',(req, res)=>{
+    return res.sendFile(__dirname + "/views/quemSomos.html");
+});
+servidor.get('/entrar.html',(req, res)=>{
+    return res.sendFile(__dirname + "/views/entrar.html");
+});
 
 // 4 - Por o servidor no modo "aguardando requisição"
 servidor.listen(3000);
